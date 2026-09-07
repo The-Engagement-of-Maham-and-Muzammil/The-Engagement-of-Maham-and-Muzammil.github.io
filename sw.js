b5ae98a1992e3fbcdc8ed3f5f731f31d17c5ce5f
The-Engagement-of-Maham-and-Muzammil <rizwanchandna.6.@gmail.com>
2026-09-08 01:11:22 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "maham-muzammil-v1";
+
+const FILES_TO_CACHE = [
+"./",
+"./index.html",
+"./manifest.json",
+"./icon-192.png",
+"./icon-512.png",
+"./photos/",
+];
+
+// Install
+self.addEventListener("install", (event) => {
+event.waitUntil(
+caches.open(CACHE_NAME).then((cache) => {
+return cache.addAll(FILES_TO_CACHE);
+})
+);
+
+self.skipWaiting();
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+event.waitUntil(
+caches.keys().then((cacheNames) => {
+return Promise.all(
+cacheNames
+.filter((name) => name !== CACHE_NAME)
+.map((name) => caches.delete(name))
+);
+})
+);
+
+self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+event.respondWith(
+caches.match(event.request).then((cachedResponse) => {
+return cachedResponse || fetch(event.request);
+})
+);
+});
